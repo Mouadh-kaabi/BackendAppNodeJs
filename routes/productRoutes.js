8113cffba906e51072b89f4b46f804fd1a500d0f
Mouadh-kaabi <kaabi.mouadh@gmail.com>
2021-12-04 20:40:56 +0100

    new push

---
+++ routes/productRoutes.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var ProductController = require('../controllers/ProductController');
+const auth = require('../middlewares/auth');
+const productImageUpload = require('../middlewares/multer.config');
+var router = express.Router();
+
+
+router.get('/', ProductController.list);
+
+router.get('/:id', ProductController.show);
+
+router.post('/', productImageUpload, ProductController.create);
+
+router.put('/:id', productImageUpload, ProductController.update);
+
+router.delete('/:id', ProductController.remove);
+
+
+module.exports = router;
